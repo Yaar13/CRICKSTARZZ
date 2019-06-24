cd67f53621d0ab18787a1a31a70e40e327c66e94
parag <paraggarg66@yahoo.in>
2019-06-24 13:28:56 -0400

    Person Class

--- CRICKSTARZZ/Person.swift
+++ CRICKSTARZZ/Person.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class Person : Display
+{
+    var personId : Int
+    var personName : String
+    var personGender : Gender
+    var personSponsor : String
+    
+    init()
+    {
+        self.personId = Int()
+        self.personName = String()
+        self.personGender = Gender.None
+        self.personSponsor = String()
+    }
+    func display()
+    {
+        print( "personId : \(personId)"+"\npersonName : \(personName)"+"\npersonGender : \(personGender)"+"\npersonSponsor : \(personSponsor)\n")
+    }
+}
