6c900565a99726d4812cc91df8c19664721caa14
parag <paraggarg66@yahoo.in>
2019-06-24 13:30:32 -0400

    Player class

--- CRICKSTARZZ/Player.swift
+++ CRICKSTARZZ/Player.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class Player : Person
+{
+    var playerType : PlayerType
+    var countryName : String
+    var playerJerseyNo : Int
+    var inPlaying : Bool
+    
+    override init()
+    {
+        self.playerType = PlayerType.None
+        self.countryName = ""
+        self.playerJerseyNo = 0
+        self.inPlaying = false
+    }
+    override func display() {
+        print( "personId : \(personId)"+"\npersonName : \(personName)"+"\npersonGender : \(personGender)"+"\npersonSponsor : \(personSponsor)\n" + "playerType : \(playerType)"+"\ncountryName : \(countryName)\n"+"\nplayerJerseyNo : \(playerJerseyNo)"+"\ninPlaying : \(inPlaying)")
+    }
+}
